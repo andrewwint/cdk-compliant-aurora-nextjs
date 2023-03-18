536447afd4cf43ad3cd073ad0540a9e4663df71e
Andrew Wint <wint.andrew@gmail.com>
2023-03-18 07:15:11 -0400

    Initial commit

---
+++ test/cdk-compliant-aurora-nextjs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkCompliantAuroraNextjs from '../lib/cdk-compliant-aurora-nextjs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-compliant-aurora-nextjs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkCompliantAuroraNextjs.CdkCompliantAuroraNextjsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
